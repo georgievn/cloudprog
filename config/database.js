d7440de7979eed3ac76df5e1186f3ff3832706a5
georgievn <40321473+georgievn@users.noreply.github.com>
2018-06-24 11:06:52 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://mongodb://cloudprogdb:yEKxfJtoH7aQks8vpiaAypcqd5dXTU3k5ajFtdzonRiTmTidE4c7xbTmfjDmK45ut8B1N2UBADtSWf0o6kDVqA==@cloudprogdb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
